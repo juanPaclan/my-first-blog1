b771f41ec910dae55b2183af4b5125d60e808c63
Juan Paclan <paclan9@hotmail.com>
2017-08-31 13:14:00 -0500

    nuevos datos

--- proyecto1/urls.py
+++ proyecto1/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
-from proyecto1.views import pro1, articulo
+from proyecto1.views import pro1, articulo, desc_arti
 from proyecto1.models import Articulo
 urlpatterns = [
 # Ejemplos:
 # url(r'^$', 'misitio.views.home', name='home'),
 # url(r'^blog/', include('blog.urls')),
-    url(r'^pro1/$', pro1),
-    url(r'^cel/$', articulo,{'model': Articulo}),
+    url(r'^pro1/$', pro1, name='index'),
+    url(r'^pro1/(?P<tipo>[^/]+)$', articulo,{'model': Articulo}, name='articulo'),
+    url(r'^pro1/(?P<tipo>[^/]+)/$', desc_arti,{'model': Articulo}, name='desc_arti'),
+    #url(r'^pro1/(?P<pk>[0-9]+)/$', desc_arti,{'model': Articulo}, name='desc_arti'),
 
 ]
--- proyecto1/views.py
+++ proyecto1/views.py
@@ -4,8 +4,13 @@ from proyecto1.models import Articulo
 import datetime
 # Create your views here.
 def pro1(request):
-    return render(request, 'index.html')
-def articulo(request, model):
-    datos_articulo = model.objects.all()
-    print(datos_articulo)
-    return render(request,'cel.html', {'datos_articulos':datos_articulo})
+    articulo = Articulo.objects.all()
+    return render(request, 'index.html',{'articulos': articulo})
+def articulo(request, model, tipo):
+    datos_articulo = model.objects.filter(producto=tipo)
+    dato = tipo
+    return render(request,'cel.html', {'datos':dato,'datos_articulos':datos_articulo})
+def desc_arti(request,model, tipo):
+    desc_dato = model.objects.filter(modelo=tipo)
+    dato = tipo
+    return render(request, 'cel-desc.html', {'datos':dato, 'desc_datos': desc_dato})
