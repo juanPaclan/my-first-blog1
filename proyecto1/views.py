114d8d88fe0339492cd5abb86e55db867cf104b2
Juan Paclan <paclan9@hotmail.com>
2017-08-31 14:30:21 -0500

    nueva ruleta

--- proyecto1/views.py
+++ proyecto1/views.py
@@ -4,8 +4,11 @@ from proyecto1.models import Articulo
 import datetime
 # Create your views here.
 def pro1(request):
-    articulo = Articulo.objects.all()
-    return render(request, 'index.html',{'articulos': articulo})
+    articulo = Articulo.objects.filter(producto='CELULAR').order_by('?')[:4]
+    articulo1 = Articulo.objects.filter(producto='CELULAR').order_by('?')[:4]
+    articulo2 = Articulo.objects.filter(producto='CELULAR').order_by('?')[:4]
+    dato = [articulo,articulo1, articulo2]
+    return render(request, 'index.html',{ 'datos':dato})
 def articulo(request, model, tipo):
     datos_articulo = model.objects.filter(producto=tipo)
     dato = tipo
